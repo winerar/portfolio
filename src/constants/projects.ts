84b97175725600c7ef21fedbe6c5226d8d7822b2
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2023-12-13 17:12:23 +0400

    add project

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,40 +1,21 @@
 import djNejtrino from "../assets/images/projects/dj_nejtrino.png"
-import { ProjectInfo, Stack } from "../types"
+import forum from "../assets/images/projects/forum.png"
+import { IProject, Stack } from "../types"
 
-export const projects: Array<ProjectInfo> = [
+export const projects: Array<IProject> = [
 	{
 		name: "Landing for DJ Nejtrino",
-		description: "Adaptive Landing for Customer",
+		description:
+			"Adaptive landing for Customer. In addition to development, I have also prepared images and icons using Photoshop and Illustrator.",
 		stack: [Stack.HTML, Stack.CSS],
 		image: djNejtrino,
 		repo: "https://github.com/winerar/dj_nejtrino",
 	},
 	{
-		name: "Landing for DJ Nejtrino",
-		description: "Adaptive Landing for Customer",
-		stack: [Stack.HTML, Stack.CSS],
-		image: djNejtrino,
-		repo: "https://github.com/winerar/dj_nejtrino",
-	},
-	{
-		name: "Landing for DJ Nejtrino",
-		description: "Adaptive Landing for Customer",
-		stack: [Stack.HTML, Stack.CSS],
-		image: djNejtrino,
-		repo: "https://github.com/winerar/dj_nejtrino",
-	},
-	{
-		name: "Landing for DJ Nejtrino",
-		description: "Adaptive Landing for Customer",
-		stack: [Stack.HTML, Stack.CSS],
-		image: djNejtrino,
-		repo: "https://github.com/winerar/dj_nejtrino",
-	},
-	{
-		name: "Landing for DJ Nejtrino",
-		description: "Adaptive Landing for Customer",
-		stack: [Stack.HTML, Stack.CSS],
-		image: djNejtrino,
-		repo: "https://github.com/winerar/dj_nejtrino",
+		name: "Forum for customer",
+		description: "Adaptive forum site for Customer.",
+		stack: [Stack.HTML, Stack.CSS, Stack.SASS, Stack.Bootstrap],
+		image: forum,
+		repo: "https://github.com/winerar/customer-forum",
 	},
 ]
