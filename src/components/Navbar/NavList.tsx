61a4ec9513e2a10be1a08d7e9779becd12edb573
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2023-12-14 12:15:07 +0400

    fix active link style

--- src/components/Navbar/NavList.tsx
+++ src/components/Navbar/NavList.tsx
@@ -1,11 +1,21 @@
 import { Nav } from "react-bootstrap"
 import { navLinks } from "../../constants"
+import { useLocation } from "react-router-dom"
 
 const NavList: React.FC = (): JSX.Element => {
+	const location = useLocation()
+
 	return (
 		<Nav className="ms-auto flex-lg-column text-lg-center">
 			{navLinks.map((item, index) => (
-				<Nav.Link href={item.path} key={index}>
+				<Nav.Link
+					href={item.path}
+					key={index}
+					active={
+						location.pathname.substring(1) === item.path.substring(1) ||
+						(location.pathname === "/" && item.path === "#home")
+					}
+				>
 					{item.name}
 				</Nav.Link>
 			))}
