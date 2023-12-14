2f8fb09cab627e631d0bb41f38c5ab500b6bff30
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2023-12-14 11:38:34 +0400

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,51 @@ import aos from "aos"
 import "./style.css"
 import Navbar from "./components/Navbar"
 import Content from "./components/Content"
+import { RouterProvider, createHashRouter } from "react-router-dom"
+import Home from "./components/Content/Home"
+import Services from "./components/Content/Services"
+import Projects from "./components/Content/Projects"
+import About from "./components/Content/About"
+import Reviews from "./components/Content/Reviews"
+import Blog from "./components/Content/Blog"
+import Contact from "./components/Content/Contact"
+
+const router = createHashRouter([
+	{
+		path: "/",
+		Component: Content,
+		children: [
+			{
+				path: "home",
+				Component: Home,
+			},
+			{
+				path: "services",
+				Component: Services,
+			},
+			{
+				path: "work",
+				Component: Projects,
+			},
+			{
+				path: "about",
+				Component: About,
+			},
+			{
+				path: "reviews",
+				Component: Reviews,
+			},
+			{
+				path: "blog",
+				Component: Blog,
+			},
+			{
+				path: "contact",
+				Component: Contact,
+			},
+		],
+	},
+])
 
 const App: React.FC = (): JSX.Element => {
 	aos.init({
@@ -20,7 +65,7 @@ const App: React.FC = (): JSX.Element => {
 	return (
 		<>
 			<Navbar />
-			<Content />
+			<RouterProvider router={router} />
 		</>
 	)
 }
