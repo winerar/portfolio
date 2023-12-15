b9e480f8ea1dc7964f0e375afa371324c2fca05d
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2023-12-15 12:31:50 +0400

    fix title

--- src/components/Content/Reviews.tsx
+++ src/components/Content/Reviews.tsx
@@ -6,7 +6,10 @@ import SectionHeader from "./SectionHeader"
 const Reviews: React.FC = (): JSX.Element => {
 	return (
 		<Section id="reviews">
-			<SectionHeader title="What My Customers Say" subtitle="Reviews" />
+			<SectionHeader
+				title="What My Customers and Colleagues Say"
+				subtitle="Reviews"
+			/>
 			<div className="row row-gap-4">
 				{reviews.map((item, index) => (
 					<Review index={index} {...item} key={index} />
