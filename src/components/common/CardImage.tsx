c9c3351cd5dec4980862902801578e210680d41d
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2023-12-20 17:23:09 +0400

    add image size

--- src/components/common/CardImage.tsx
+++ src/components/common/CardImage.tsx
@@ -3,7 +3,7 @@ import { ICardImageProps } from "../../types"
 const CardImage: React.FC<ICardImageProps> = ({ src, alt }): JSX.Element => {
 	return (
 		<div className="card-custom__image rounded-4 w-100">
-			<img loading="lazy" src={src} alt={alt} className="rounded-4" />
+			<img loading="lazy" src={src} alt={alt} className="rounded-4" width={"512px"} height={"252.533px"} />
 		</div>
 	)
 }
