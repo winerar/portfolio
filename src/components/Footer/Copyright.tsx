b92c539b8752776b7ffc499ff6cb0ae87e0219c7
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2024-01-15 16:54:23 +0400

    fix copyright

--- src/components/Footer/Copyright.tsx
+++ src/components/Footer/Copyright.tsx
@@ -1,5 +1,5 @@
 const Copyright: React.FC = (): JSX.Element => {
-	return <p className="mb-0">© 2023-2024 Alexander Karachevtsev</p>
+	return <p className="mb-0">© 2024 Alexander Karachevtsev</p>
 }
 
 export default Copyright
