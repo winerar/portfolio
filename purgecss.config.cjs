aaf08c6285e327dcab14322f01ea647f05b6fe04
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2024-02-15 11:38:48 +0400

    fix purgecss config

--- purgecss.config.cjs
+++ purgecss.config.cjs
@@ -2,6 +2,7 @@
 module.exports = {
   content: ['./dist/*.html', './dist/assets/*.js'],
   css: ['./dist/assets/*.css'],
+  output: "./dist/assets/",
   safelist: {
     standard: [/active/, "navbar-collapse", /collapse/, "collapsing", /show/],
     deep: [/^dropdown/, /^modal/, /^carousel/, /collapse/, /navbar/]
