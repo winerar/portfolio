ac15ef526152fccd103a2305b9404975558e69de
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2024-02-17 17:37:08 +0400

    add title attribute in social

--- src/components/Footer/Social.tsx
+++ src/components/Footer/Social.tsx
@@ -6,13 +6,13 @@ import { contact } from "../../constants"
 const Social: React.FC = (): JSX.Element => {
 	return (
 		<div className="d-flex gap-3">
-			<a href={contact.telegram} target="_blank">
+			<a href={contact.telegram} target="_blank" title="Telegram">
 				<Telegram className="icon-big" />
 			</a>
-			<a href={contact.linkedIn} target="_blank">
+			<a href={contact.linkedIn} target="_blank" title="LinkedIn">
 				<LinkedIn className="icon-big" />
 			</a>
-			<a href={contact.github} target="_blank">
+			<a href={contact.github} target="_blank" title="GitHub">
 				<GitHub className="icon-big" />
 			</a>
 		</div>
